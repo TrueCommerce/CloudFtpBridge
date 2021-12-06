ea2c4b56a29147c706aa9798f6c345852d7bdb9b
Josh Johnson <josh.johnson@truecommerce.com>
2021-12-06 14:14:47 -0500

    Remove Commented Code

--- src/CloudFtpBridge.Infrastructure.FTP/FTPFileSystem.cs
+++ src/CloudFtpBridge.Infrastructure.FTP/FTPFileSystem.cs
@@ -81,54 +81,5 @@ namespace CloudFtpBridge.Infrastructure.FTP
 
             await Task.Run(()=>_ftpClient.SendToFtp(fileName, fromStream));
         }
-
-        //public async Task Send(string localPath)
-        //{
-        //   await Task.Run(() => SendFiles(localPath));
-        //}
-
-        //public async Task Receive(string localPath)
-        //{
-        //    await Task.Run(() => ReceiveFiles(localPath));
-        //}
-
-        //public void SendFiles(string localPath)
-        //{
-        //    Send ftpSend = new Send();
-        //    ftpSend.Pass = _options.Password;
-        //    ftpSend.User = _options.Username;
-        //    ftpSend.FtpUrl = _options.Host;
-        //    ftpSend.LocalDestDir = localPath;
-        //    ftpSend.Dir = _options.Path;
-        //    ftpSend.UseFtps = _options.UseFtps;
-        //    ftpSend.Port = _options.Port;
-
-        //    if (_options.AutoConnect || (!string.IsNullOrEmpty(_options.DataConnectionType) && _options.DataConnectionType.Equals("Passive")))
-        //        ftpSend.UsePassive = true;
-        //    else
-        //        ftpSend.UsePassive = false;
-
-        //    ftpSend.SendFiles();
-        //}
-
-        //public void ReceiveFiles(string localPath)
-        //{
-        //    Receive ftpRcv = new Receive();
-        //    ftpRcv.Pass = _options.Password;
-        //    ftpRcv.User = _options.Username;
-        //    ftpRcv.FtpUrl = _options.Host;
-        //    ftpRcv.LocalDestDir = localPath;
-        //    ftpRcv.Dir = _options.Path;
-        //    ftpRcv.UseFtps = _options.UseFtps;
-        //    ftpRcv.Port = _options.Port;
-
-        //    if (_options.AutoConnect || (!string.IsNullOrEmpty(_options.DataConnectionType) && _options.DataConnectionType.Equals("Passive")))
-        //        ftpRcv.SetPassive = true;
-        //    else
-        //        ftpRcv.SetPassive = false;
-
-        //    ftpRcv.Download();
-        //}
-
     }
 }
